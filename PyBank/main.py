67a11ebe24799047689da838a197b2b56b11db24
楊思婷 <christineyang@youomous-MacBook-Air.local>
2022-12-17 19:33:32 -0800

    Updated Readme file

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,15 +2,21 @@ import os
 import csv
 print("Financial Analysis")
 print("----------------------------")
+
+# csv path:
 find_csv = "/Users/christineyang/Desktop/challenge3/python-challenge/PyBank/Resources/budget_data.csv"
 month = 0
 profit_loss = 0
 profit_list = []
 month_list = []
 value = 0 
+#open csv file
 with open(find_csv, "r") as open_file:
     reader = csv.reader(open_file, delimiter=",")
+    # skip the headers
     next(open_file, None)
+    # get the total month, total amount of profit/loss
+    # add each month value and profit/loss to seperate list
     for row in reader:
         month += 1
         profit_loss += int(row[1])
@@ -20,12 +26,12 @@ with open(find_csv, "r") as open_file:
     
     print("Total Months: " + str(month))
     print("Total: $" +str(profit_loss))
-    # print(int(profit_list[month-1]))
-    # print(int(profit_list[0]))
+    # calculate the changes of profit/loss over the entire period and the average of those changes
     change = int(profit_list[month-1]) - int(profit_list[0])
     change_average = round((change / month), 2)
     print("Average Change: $" + str(change_average))
 
+# find the greatest increase in profits (date and amount) over the entire period
 value = 0
 i = 1
 while i <= int(month - 1):
@@ -37,7 +43,10 @@ while i <= int(month - 1):
         value = compare_big
         date = month_list[i]
     i += 1
+
 print("Greatest Increase in Profits: " + date + " ($" + str(value) + ")")
+
+#find the greatest decrease in profits (date and amount) over the entire period
 svalue = 0
 j = 1
 while j <= int(month - 1):
@@ -53,6 +62,7 @@ while j <= int(month - 1):
 
 print("Greatest Decrease in Profits: " + sdate + " ($" + str(svalue) + ")")
 
+# create a txt file for the results
 a = "/Users/christineyang/Desktop/challenge3/python-challenge/PyBank/analysis"
 write_path = os.path.join(a, "PyBank.txt")
 lines = ["Financial Analysis","----------------------------","Total Months: " + str(month),
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,10 @@
 import os
 import csv
 
+# csv path
 path = "/Users/christineyang/Desktop/challenge3/python-challenge/PyPoll/Resources/election_data.csv"
 total = 0
+# c : Charles Casper Stockham, d : Diana DeGette, r: Raymon Anthony Doane
 c = 0 
 d = 0
 r = 0
@@ -13,10 +15,13 @@ candidate = []
 
 print("Election Results")
 print("-------------------------")
+
+# open csv file and read
 with open(path, "r") as open_file :
     reader = csv.reader(open_file, delimiter=",")
+    # skip the headers
     header = next(reader)
-    
+    # calculate the total volume of votes cast, and the total number of votes each candidate got
     for row in reader:
         total += 1
         candidate.append(row[2])
@@ -26,7 +31,8 @@ with open(path, "r") as open_file :
             d += 1
         else:
             r += 1
-    
+    # The percentage of votes each candidate won
+    # c : Charles Casper Stockham, d : Diana DeGette, r: Raymon Anthony Doane
     c_percentage = round(c / total * 100, 3)
     d_percentage = round(d / total * 100, 3)
     r_percentage = round(r / total * 100, 3)
@@ -39,6 +45,7 @@ with open(path, "r") as open_file :
     print(f"{r_name}: {r_percentage}% ({r})")
     print("-------------------------")
     
+    # find the winner of the election based on popular vote
     if c_percentage > d_percentage and c_percentage > r_percentage:
         a = f"Winner: {c_name}"
         print(f"Winner: {c_name}")
@@ -53,6 +60,7 @@ with open(path, "r") as open_file :
     print("-------------------------")
 
 
+# create a txt file for the results
 analysis = "/Users/christineyang/Desktop/challenge3/python-challenge/PyPoll/analysis"
 path2 = os.path.join(analysis, "PyPoll.txt")
 with open(path2, "w") as f:
